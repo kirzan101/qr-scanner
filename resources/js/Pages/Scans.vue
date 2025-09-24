8847d3b284cf787b80bd51ffba18fca5160f376a
jenreyastoria <jenrey.galanida@astoriaplaza.com>
2025-09-24 10:22:44 +0800

    created Scan UI and registered in app provider

---
+++ resources/js/Pages/Scans.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <ScanContent :errors="errors" :flash="flash" :can="can" />
+    </v-container>
+</template>
+<script setup>
+import ScanContent from "../Components/Pages/Scan/ScanContent.vue";
+
+const props = defineProps({
+    errors: String,
+    flash: String,
+    can: Array,
+});
+</script>
